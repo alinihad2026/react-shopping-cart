ba6aa5248c588a7cdbf37f53b6e1570476c56fe9
alinihad2026 <alinihadmerza@yahoo.com>
2025-05-07 18:12:54 +0300

    footer component main

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -5,3 +5,14 @@ const sass=gulpSass(require('sass'));//turn on compiler gulp
 gulp.task('sass',async function(){
     gulp.src('src/components/**/*.scss').pipe(sass()).pipe(gulp.dest('src/css'))
 })
+
+
+// gulp.task('sass',async function(){
+//     gulp.src('src/components/**/*.scss').pipe(sass()).pipe(gulp.dest('src/css'))
+// })
+
+gulp.task('watch',async function(){ //npm run watchتسوي تنفيذ 
+    gulp.watch('src/components/**/*.scss',async function(){//تسوي مراقية اي تفيير يحدث في هذا المسار 
+        gulp.src('src/components/**/*.scss').pipe(sass()).pipe(gulp.dest('src/css'))// src/cssتراقب وتسجل الاحداث في هذا المسار الي نفسة الفوك وبعدين تمرره عبر الكومبايلر الساس ليقوم وتحطة جوه هذا المسار
+    })
+})
---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import "../../css/Footer/Footer.css"
+function Footer() {
+  return (
+    <footer>Footer</footer>
+  )
+}
+
+export default Footer
