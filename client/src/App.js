5d3ae639022938e54e795655495dc29ff8cc88ce
alinihad2026 <alinihadmerza@yahoo.com>
2025-05-08 18:17:22 +0300

    product component

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,26 @@
+import { useState } from "react";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
-import {words} from "./words"
-
+// import {words} from "./words"
+import data from "./data.json"
+import Products from "./components/products/Products";
 function App() {
+  const [products,setProducts]=useState(data);
+  // console.log(products)
   return (
     <div className="layout">
       <Header/>
-      <main>{words.content}</main>
+      <main>
+        <div className="wrapper">
+       <Products products={products}/> 
+        <div className="wrapper-filter">filters</div>
+
+        </div>
+      </main>
       <Footer/>
     </div>
   );
 }
 
 export default App;
+// productsاي اسم نخترعه={products الستيت هذا }
